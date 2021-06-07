fc0b7928a81a5cbbe9270949e6edd3c2f6442501
jet townsend <jet@functionalprototype.com>
2021-06-07 12:48:24 -0400

    CCS881 improvements
    Add code to initialize CCS881 with baseline value
    
    Change threshold for bad values to 2**14
    
    Add lots of logging code

--- sensor-box.py
+++ sensor-box.py
@@ -20,21 +20,53 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import constants
-import logger
 import os
-
-import qwiic_ccs811
-import qwiic_bme280
 import time
 import sys
 
+import qwiic_ccs811
+import qwiic_bme280
 import smbus
+
+import constants
+import logger
 
 DEBUG=True
 HASPM25=False
 
-today = 1
+def checkBaseline(baseValues):
+    if (baseValues[0]
+        == baseValues[1]
+        == baseValues[2]
+        == baseValues[3]
+        == baseValues[4]
+        == baseValues[5]):
+        if (DEBUG):
+            logger.logEvent("all baselines equal")
+        return True
+
+    min = 65530 # initialization level
+    max = 0
+    for i in baseValues:
+        logger.logEvent ("baseline " + str(i))
+        if (i == 0):
+            logger.logEvent ("0, returning False")
+            return False
+        if (i <= min):
+            min = i
+            logger.logEvent ("new min " + str(min))
+        if (i >= max):
+            max = i
+            logger.logEvent ("new max " + str(max))
+
+    logger.logEvent("min max " + str(min) + " " + str(max))
+    noise = (abs (max - min))
+    if (noise < 2):
+        if (DEBUG):
+            logger.logEvent("noise " + str(noise) + " baselines " + str(baseValues))
+        return True
+
+    return False
 
 def main():
     os.chdir("/home/pi/Projects/sensor-box")
@@ -47,33 +79,55 @@ def main():
     pm10count = -1
     pm25count = -1
     if (ccs811Sensor.connected == False) or (bme280Sensor.connected == False):
-        print("failed to connect to sensors")
+        logger.logEvent("failed to connect to sensors")
         sys.exit(-1)
 
     ccs811Sensor.begin()
     bme280Sensor.begin()
 
     yesterday = 0
+    today = 1
+    baseValues = [ 0, 0, 0, 0, 0, 0]
+    baseValueSet = False
+    baseIndex = 0
     while True:
         today = time.localtime().tm_mday
         if (yesterday != today):
             if (DEBUG):
                 print("yesterday", yesterday, "not equal to today",today)
+                logger.logEvent("yesterday "  + str(yesterday) + " not equal to today " + str(today))
             uptime = os.popen('uptime -s').read() [:-1]
+# TODO: should we log the time the script started?
             logger.logSensorHeader("#boot time " + uptime)
             logger.logSensorHeader("time,temp,humid,CO2,tVOC,PM1.0,PM2.5,PM10.0")
             logger.logEventHeader("#boot time " + uptime)
             logger.logEventHeader("#description of event")
 
             baseline = ccs811Sensor.get_baseline()
-            logger.logEvent("#baseline " + str(baseline))
-            if (DEBUG):
-                print ("baseline ", baseline)
+            logger.logEvent("#baseline at day rollover " + str(baseline))
             yesterday = today
 
-        if ((time.localtime().tm_min % 10) == 0):
-            baseline = ccs811Sensor.get_baseline()
-            logger.logEvent("#baseline " + str(baseline))
+# TODO: should we store this value and re-use it every time a day rolls over?
+#       or should we restart the CCS881?
+        if (baseValueSet == False):
+            interval = 10
+            if ((time.localtime().tm_min % interval) == 0):
+                baseline = ccs811Sensor.get_baseline()
+                logger.logEvent("#baseline " + str(interval) + " min read " + str(baseline))
+                baseValues[baseIndex] = baseline
+                baseIndex +=1
+                logger.logEvent("#basevalues " + str(baseValues))
+                if (baseIndex == 6):
+                    baseIndex = 0
+                if (checkBaseline(baseValues)):
+                    logger.logEvent("#baseline set to " + str(baseline))
+                    ccs811Sensor.set_baseline(baseline)
+                    baseValueSet = True
+        else:
+            interval = 60
+            if ((time.localtime().tm_min % interval) == 0):
+                baseline = ccs811Sensor.get_baseline()
+                logger.logEvent("#baseline " + str(interval) + " min read " + str(baseline))
 
         humidity = bme280Sensor.humidity
         #my stretto reads 4C lower than the reported temperature
@@ -89,10 +143,10 @@ def main():
 
         if ccs811Sensor.data_available():
             ccs811Sensor.read_algorithm_results()
-            if (ccs811Sensor.CO2 > 2**15):
+            if (ccs811Sensor.CO2 > 2**14):
                 logString = '#error CO2 {:.2f}'.format(ccs811Sensor.CO2)
                 logger.logEvent(logString)
-            elif (ccs811Sensor.TVOC > 2**15):
+            elif (ccs811Sensor.TVOC > 2**14):
                 logString = '#error tVOC {:.2f}'.format(ccs811Sensor.TVOC)
                 logger.logEvent(logString)
             else:
@@ -104,7 +158,6 @@ def main():
                 logger.logSensor(logString)
                 if DEBUG:
                     print(logString)
-
         time.sleep(60)
 
 
